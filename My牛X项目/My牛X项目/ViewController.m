b15846412bea856139ac3b65ca4062f31e7718a6
IvanLiugb <77331283@qq.com>
2016-01-29 09:23:29 +0800

    Revert "22222"
    This reverts commit acdfe3a7231c0639b56917803c310dbc73c62d21.

--- My牛X项目/My牛X项目/ViewController.m
+++ My牛X项目/My牛X项目/ViewController.m
@@ -16,7 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    NSLog(@"21312312312");
+    // Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)didReceiveMemoryWarning {
